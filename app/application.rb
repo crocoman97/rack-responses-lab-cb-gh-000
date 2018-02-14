079a1e414bb285551afdb6882e75026d3b69b653
Rauof M <abdelrauof197@gmail.com>
2018-02-14 03:44:52 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new 
     hourly_time_now = Time.new.hour.to_i
     if hourly_time_now < 12 
-      resp.write "Good Morning!\n "
+      resp.write "Good Morning!\n and Afternoon will be 12AM"
     else 
       resp.write "Good Afternoon!"
     end
